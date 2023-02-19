fa994946616e62b337ef020f3741b5ce5d0d12b1
P N Vinesh Kumar <Vinesh.KumarPN@maersk.com>
2023-02-19 17:05:01 +0530

    Removed unwanted pages

--- PremiumCalculator/ClientApp/src/AppRoutes.js
+++ PremiumCalculator/ClientApp/src/AppRoutes.js
@@ -1,21 +1,6 @@
-import { Counter } from "./components/Counter";
-import { FetchData } from "./components/FetchData";
 import { PremiumCalculator } from "./components/PremiumCalculator";
-import { Home } from "./components/Home";
 
 const AppRoutes = [
-  {
-    index: true,
-    element: <Home />
-  },
-  {
-    path: '/counter',
-    element: <Counter />
-  },
-  {
-    path: '/fetch-data',
-    element: <FetchData />
-  },
   {
     path: '/premium',
     element: <PremiumCalculator />
--- PremiumCalculator/ClientApp/src/components/NavMenu.js
+++ PremiumCalculator/ClientApp/src/components/NavMenu.js
@@ -25,24 +25,8 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">PremiumCalculator</NavbarBrand>
+          <NavbarBrand tag={Link} to="/premium">PremiumCalculator</NavbarBrand>
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-            <ul className="navbar-nav flex-grow">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/premium">PremiumCalculator</NavLink>
-              </NavItem>
-            </ul>
-          </Collapse>
         </Navbar>
       </header>
     );
